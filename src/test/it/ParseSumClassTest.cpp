783ac2c93019e1b72c4845be1e350fb33f3ca398
Chamber6821 <vania.osinin@yandex.ru>
2024-03-31 01:30:28 +0300

    test(it/ParseSumClassTest): add subcase Count of constants must be equal 22

--- src/test/it/ParseSumClassTest.cpp
+++ src/test/it/ParseSumClassTest.cpp
@@ -3,11 +3,16 @@
 #include <fstream>
 
 TEST_SUITE("it") {
-    TEST_CASE("Print structure") {
+    TEST_CASE("Parse resource/java/Sum") {
         std::system("javac ./resource/java/Sum.java");
         std::ifstream classFile(
             "./resource/java/Sum.class",
             std::ifstream::binary
         );
+        const auto klass = ClassFromFile(classFile);
+
+        SUBCASE("Count of constants must be equal 22") {
+            REQUIRE(klass.constants().count() == 22);
+        }
     }
 }
