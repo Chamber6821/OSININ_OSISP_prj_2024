4a3a2f8c7820fd3f5077719bef6b3da5b00433f9
Chamber6821 <vania.osinin@yandex.ru>
2024-03-31 02:15:32 +0300

    refactor(it/ParseSumClassTest): change style of test

--- src/test/it/ParseSumClassTest.cpp
+++ src/test/it/ParseSumClassTest.cpp
@@ -5,14 +5,13 @@
 TEST_SUITE("it") {
     TEST_CASE("Parse resource/java/Sum") {
         std::system("javac ./resource/java/Sum.java");
-        std::ifstream classFile(
-            "./resource/java/Sum.class",
-            std::ifstream::binary
-        );
-        const auto klass = ClassFromFile(classFile);
-
         SUBCASE("Count of constants must be equal 22") {
-            REQUIRE(klass.constants().count() == 22);
+            REQUIRE(
+                make<CzFile>("./resource/java/Sum.class")
+                    ->constants()
+                    ->count() == 22
+            );
         }
+        std::system("rm ./resource/java/Sum.class");
     }
 }
