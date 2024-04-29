70ce7b3a6db1b97ea0bdcb3f237861d76bc0a520
Chamber6821 <vania.osinin@yandex.ru>
2024-04-29 23:28:44 +0300

    feat: add CpMutable

---
+++ src/main/class-file/constant/pool/CpMutable.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "class-file/constant/Constant.hpp"
+#include "class-file/constant/pool/ConstantPool.hpp"
+#include <exception>
+#include <format>
+#include <map>
+#include <stdexcept>
+
+class CpMutable : public ConstantPool {
+  std::map<int, p<Constant>> map;
+
+public:
+  CpMutable(std::map<int, p<Constant>> map) : map(std::move(map)) {}
+
+  CpMutable() : CpMutable(std::map<int, p<Constant>>{}) {}
+
+  p<Constant> at(int index) const override try {
+    return map.at(index);
+  } catch (...) {
+
+    std::throw_with_nested(std::runtime_error(
+      std::format("Constant with index {} does not exists", index)
+    ));
+  }
+
+  void add(int index, p<Constant> constant) { map[index] = constant; }
+};
