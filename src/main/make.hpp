a56e1abe15207e332ebd5906ef37ab2d626d9015
Chamber6821 <vania.osinin@yandex.ru>
2024-04-29 22:16:11 +0300

    fix(make): correct use std::forward with type pack

--- src/main/make.hpp
+++ src/main/make.hpp
@@ -4,5 +4,5 @@
 
 template <class T, class... Args>
 auto make(Args &&...args) {
-  return std::make_shared<T>(std::forward<T>(args)...);
+  return std::make_shared<T>(std::forward<Args>(args)...);
 }
