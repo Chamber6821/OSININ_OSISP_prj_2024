9b3b7b29481e27bb818d82dad29d53e8606484d7
Chamber6821 <vania.osinin@yandex.ru>
2024-04-30 14:10:38 +0300

    fix(CoScalarParser): use correct return typ

--- src/main/class-file/parser/constant/CoScalarParser.hpp
+++ src/main/class-file/parser/constant/CoScalarParser.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "class-file/constant/CoInteger.hpp"
 #include "class-file/constant/CoScalar.hpp"
 #include "class-file/constant/Constant.hpp"
 #include "class-file/parser/constant/CoParser.hpp"
@@ -10,10 +9,10 @@
 template <Constant::Tag TAG, class T>
 class CoScalarParser : public CoParser<TAG, CoScalar<TAG, T>> {
 public:
-  p<T> parsed(std::istream &in) const override {
+  p<CoScalar<TAG, T>> parsed(std::istream &in) const override {
     in.get();
     T value = 0;
     in.read((char *)&value, sizeof(value));
-    return make<CoInteger>(std::move(value));
+    return make<CoScalar<TAG, T>>(std::move(value));
   }
 };
