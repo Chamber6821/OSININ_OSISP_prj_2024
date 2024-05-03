46fa0e83e9e5bc70e8f2aeebdac1fee32aa6c942
Chamber6821 <vania.osinin@yandex.ru>
2024-05-03 14:50:16 +0300

    feat(Context): add arguments()

--- src/main/code/context/Context.hpp
+++ src/main/code/context/Context.hpp
@@ -10,4 +10,5 @@ public:
   virtual std::uint64_t executionPoint() const = 0;
   virtual p<StackFrame> stack() const = 0;
   virtual p<Locals> locals() const = 0;
+  virtual p<Locals> arguments() const = 0;
 };
