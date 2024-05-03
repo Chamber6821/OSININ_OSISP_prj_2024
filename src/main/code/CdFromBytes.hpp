719b3b2876c724881e37636737d7350736afd2d2
Chamber6821 <vania.osinin@yandex.ru>
2024-05-03 15:49:24 +0300

    feat: add CdFromBytes

---
+++ src/main/code/CdFromBytes.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "code/Code.hpp"
+#include "code/context/Context.hpp"
+#include "code/instruction-set/InstructionSet.hpp"
+#include "p.hpp"
+#include <cstdint>
+#include <variant>
+#include <vector>
+
+class CdFromBytes : public Code {
+  std::vector<std::uint8_t> bytes;
+  p<InstructionSet> instructionSet;
+
+public:
+  CdFromBytes(std::vector<std::uint8_t> bytes, p<InstructionSet> instructionSet)
+      : bytes(std::move(bytes)), instructionSet(std::move(instructionSet)) {}
+
+  Result result(p<Context> context) const override {
+    Result result = Next{};
+    while (std::holds_alternative<Next>(result)) {
+      result = instructionSet
+                 ->instructionFor(
+                   {bytes.begin() + context->instructionPointer()->address(),
+                    bytes.end()}
+                 )
+                 ->result(context);
+    }
+    return result;
+  }
+};
