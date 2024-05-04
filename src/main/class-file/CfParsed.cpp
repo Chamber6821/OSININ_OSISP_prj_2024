ad0b79ed5f29707f0093fffedebe820384797793
Chamber6821 <vania.osinin@yandex.ru>
2024-05-05 01:05:15 +0300

    feat(CfParsed): add try catch

--- src/main/class-file/CfParsed.cpp
+++ src/main/class-file/CfParsed.cpp
@@ -3,8 +3,10 @@
 #include "class-file/fields/FsParsed.hpp"
 #include "class-file/interfaces/IeParsed.hpp"
 #include "class-file/methods/MsParsed.hpp"
+#include <format>
+#include <stdexcept>
 
-CfParsed::CfParsed(std::istream &in) {
+CfParsed::CfParsed(std::istream &in) try {
   in.seekg(8, std::ios::cur);
   pool = make<CpParsed>(in);
   in.seekg(2, std::ios::cur);
@@ -14,6 +16,11 @@ CfParsed::CfParsed(std::istream &in) {
   _fields = make<FsParsed>(in, pool);
   _methods = make<MsParsed>(in, pool);
   _attributes = make<AsParsed>(in, pool);
+} catch (...) {
+
+  std::throw_with_nested(std::runtime_error(
+    std::format("Failed while parse class file at position {}", (int)in.tellg())
+  ));
 }
 
 p<CoClass> CfParsed::thisClass() const {
