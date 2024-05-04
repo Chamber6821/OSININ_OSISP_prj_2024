0f1a59786143fa6b292fba887006a6458649e485
Chamber6821 <vania.osinin@yandex.ru>
2024-05-04 14:16:12 +0300

    feat(JoMap): implement type()

--- src/main/java/object/JoMap.hpp
+++ src/main/java/object/JoMap.hpp
@@ -1,15 +1,21 @@
 #pragma once
 
+#include "java/class/JavaClass.hpp"
 #include "java/object/JavaObject.hpp"
 #include "java/value/JavaValue.hpp"
 #include "p.hpp"
 #include <map>
+#include <stdexcept>
 #include <string>
 
 class JoMap : public JavaObject {
   std::map<std::string, p<JavaValue>> map;
 
 public:
+  p<JavaClass> type() const override {
+    throw std::runtime_error("I am stupid box with data without type");
+  }
+
   p<JavaValue> field(std::string name) const override { return map.at(name); }
 
   void setField(std::string name, p<JavaValue> value) override {
