dd8a198057f5daf3228cf96cfd455ded8ca09e37
Chamber6821 <vania.osinin@yandex.ru>
2024-05-04 19:02:10 +0300

    feat(JavaClass): add name()

--- src/main/java/class/JavaClass.hpp
+++ src/main/java/class/JavaClass.hpp
@@ -8,6 +8,7 @@ class JavaObject;
 
 class JavaClass : public Interface {
 public:
+  virtual std::string name() const = 0;
   virtual p<JavaObject> newObject(p<JavaClass> type) const = 0;
   virtual p<Code> methodCode(std::string name, std::string signature) const = 0;
 };
