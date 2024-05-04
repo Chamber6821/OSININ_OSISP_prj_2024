5573091e500d988fcbf5f72f83f0f1352742733b
Chamber6821 <vania.osinin@yandex.ru>
2024-05-05 01:56:07 +0300

    fix(asRange): correct use std::views::iota()

--- src/main/tool/asRange.hpp
+++ src/main/tool/asRange.hpp
@@ -4,6 +4,6 @@
 
 template <class L>
 auto asRange(const L &list) {
-  return std::views::iota(list.count()) |
+  return std::views::iota(0, list.count()) |
          std::views::transform([&](int i) { return list.at(i); });
 }
