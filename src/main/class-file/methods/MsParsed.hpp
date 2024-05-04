e56dbda4b50490754680004ff38163932e3ac8c2
Chamber6821 <vania.osinin@yandex.ru>
2024-05-05 01:52:07 +0300

    feat(MsParsed): add try catch

--- src/main/class-file/methods/MsParsed.hpp
+++ src/main/class-file/methods/MsParsed.hpp
@@ -7,19 +7,24 @@
 #include "make.hpp"
 #include "tool/readInt.hpp"
 #include <cstdint>
+#include <exception>
 #include <istream>
+#include <stdexcept>
 #include <vector>
 
 class MsParsed : public Methods {
   std::vector<p<Method>> methods;
 
 public:
-  MsParsed(std::istream &in, p<ConstantPool> pool) {
+  MsParsed(std::istream &in, p<ConstantPool> pool) try {
     std::uint16_t count;
     readInt(in, count);
     methods.assign(count, nullptr);
     for (auto &method : methods)
       method = make<MeParsed>(in, pool);
+  } catch (...) {
+
+    std::throw_with_nested(std::runtime_error("Failed while parse methods"));
   }
 
   int count() const override { return methods.size(); }
