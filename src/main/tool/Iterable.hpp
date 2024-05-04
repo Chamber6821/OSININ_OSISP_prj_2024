0c01e87e1167a6ddbd315b6dcf6215e6f38e494e
Chamber6821 <vania.osinin@yandex.ru>
2024-05-05 00:58:30 +0300

    fix(Iterable): define inner classes outside

--- src/main/tool/Iterable.hpp
+++ src/main/tool/Iterable.hpp
@@ -12,51 +12,58 @@ template <class T>
 class Iterable : public Interface {
 
 public:
-  class Empty : public Iterable {
-  public:
-    bool hasNext() const override { return false; }
-
-    T next() override {
-      throw std::runtime_error("Iterable::Empty is always empty");
-    }
-  };
-
-  class Single : public Iterable {
-    std::optional<T> element;
-
-  public:
-    Single(T element) : element(std::move(element)) {}
-
-    bool hasNext() const override { return element.has_value(); }
-
-    T next() override {
-      if (not element.has_value())
-        throw std::runtime_error("Iterable::Single already gave element away");
-      return std::move(element);
-    }
-  };
-
-  class Sequence : public Iterable {
-    std::queue<T> elements;
-
-  public:
-    Sequence(std::queue<T> elements) : elements(std::move(elements)) {}
-
-    template <class... Elements>
-    Sequence(Elements &&...elements) : Sequence(std::deque<T>{elements...}) {}
-
-    bool hasNext() const override { return not elements.empty(); }
-
-    T next() override {
-      if (elements.empty())
-        throw std::runtime_error("Iterable::Sequence is exhausted");
-      T element = std::move(elements.front());
-      elements.pop();
-      return element;
-    }
-  };
+  class Empty;
+  class Single;
+  class Sequence;
 
   virtual bool hasNext() const = 0;
 
   virtual T next() = 0;
+};
+
+template <class T>
+class Iterable<T>::Empty : public Iterable {
+public:
+  bool hasNext() const override { return false; }
+
+  T next() override {
+    throw std::runtime_error("Iterable::Empty is always empty");
+  }
+};
+
+template <class T>
+class Iterable<T>::Single : public Iterable {
+  std::optional<T> element;
+
+public:
+  Single(T element) : element(std::move(element)) {}
+
+  bool hasNext() const override { return element.has_value(); }
+
+  T next() override {
+    if (not element.has_value())
+      throw std::runtime_error("Iterable::Single already gave element away");
+    return std::move(element);
+  }
+};
+
+template <class T>
+class Iterable<T>::Sequence : public Iterable {
+  std::queue<T> elements;
+
+public:
+  Sequence(std::queue<T> elements) : elements(std::move(elements)) {}
+
+  template <class... Elements>
+  Sequence(Elements &&...elements) : Sequence(std::deque<T>{elements...}) {}
+
+  bool hasNext() const override { return not elements.empty(); }
+
+  T next() override {
+    if (elements.empty())
+      throw std::runtime_error("Iterable::Sequence is exhausted");
+    T element = std::move(elements.front());
+    elements.pop();
+    return element;
+  }
 };
