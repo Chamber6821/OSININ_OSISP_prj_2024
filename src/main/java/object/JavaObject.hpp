e1ed6cdffd6b2eda6ea02b9c29352306d2497413
Chamber6821 <vania.osinin@yandex.ru>
2024-05-04 13:26:09 +0300

    feat(JavaObject): add type() for virtual calls

--- src/main/java/object/JavaObject.hpp
+++ src/main/java/object/JavaObject.hpp
@@ -2,11 +2,13 @@
 
 #include "Interface.hpp"
 #include "class-file/constant/CoFieldRef.hpp"
+#include "java/class/JavaClass.hpp"
 #include "java/value/JavaValue.hpp"
 #include "p.hpp"
 
 class JavaObject : public Interface {
 public:
+  virtual p<JavaClass> type() const = 0;
   virtual p<JavaValue> field(p<CoFieldRef> ref) const = 0;
   virtual void setField(p<CoFieldRef> ref, p<JavaValue> value) = 0;
 };
