da2f254ad737d28dd0abf57616a14120b0deefde
Chamber6821 <vania.osinin@yandex.ru>
2024-05-04 15:36:44 +0300

    refactor(JavaClass): replace CoNameAndType with strings

--- src/main/java/class/JavaClass.hpp
+++ src/main/java/class/JavaClass.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "Interface.hpp"
-#include "class-file/constant/CoNameAndType.hpp"
 #include "p.hpp"
 
 class Code;
@@ -10,5 +9,5 @@ class JavaObject;
 class JavaClass : public Interface {
 public:
   virtual p<JavaObject> newObject(p<JavaClass> type) const = 0;
-  virtual p<Code> methodCode(p<CoNameAndType> ref) const = 0;
+  virtual p<Code> methodCode(std::string name, std::string signature) const = 0;
 };
