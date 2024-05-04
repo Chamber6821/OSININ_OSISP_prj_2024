0ad7aa74fc2988a52d6824355c4a4c8f39d2b65a
Chamber6821 <vania.osinin@yandex.ru>
2024-05-04 14:26:03 +0300

    feat(JavaClass): replace newObject() with newObject(p<JavaClass> type)

--- src/main/java/class/JavaClass.hpp
+++ src/main/java/class/JavaClass.hpp
@@ -9,6 +9,6 @@ class JavaObject;
 
 class JavaClass : public Interface {
 public:
-  virtual p<JavaObject> newObject() const = 0;
+  virtual p<JavaObject> newObject(p<JavaClass> type) const = 0;
   virtual p<Code> methodCode(p<CoNameAndType> ref) const = 0;
 };
