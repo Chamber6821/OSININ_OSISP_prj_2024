6a2e4a4e01931cd1942cc4aaad95901bec30f0d2
Chamber6821 <vania.osinin@yandex.ru>
2024-05-05 11:46:50 +0300

    feat(Code): add Wrap

--- src/main/code/Code.hpp
+++ src/main/code/Code.hpp
@@ -3,9 +3,13 @@
 #include "Interface.hpp"
 #include "code/context/Context.hpp"
 #include "java/class/JavaClass.hpp"
+#include "java/object/JavaObject.hpp"
 #include "java/value/JavaValue.hpp"
 #include "java/value/JavaValues.hpp"
 #include "p.hpp"
+#include <algorithm>
+#include <functional>
+#include <utility>
 #include <variant>
 
 class Code : public Interface {
@@ -31,9 +35,22 @@ public:
 
   using Result = std::variant<Next, ReturnVoid, ReturnValue, Call, Throw>;
 
+  class Wrap;
+
   virtual Result result(p<Context> context, p<JavaObject> exception) const = 0;
 
   Result result(p<Context> context) const {
     return result(std::move(context), nullptr);
+  }
+};
+
+class Code::Wrap : public Code {
+  std::function<Result(p<Context>, p<JavaObject>)> implementation;
+
+public:
+  Wrap(auto implementation) : implementation(std::move(implementation)) {}
+
+  Result result(p<Context> context, p<JavaObject> exception) const override {
+    return implementation(std::move(context), std::move(exception));
   }
 };
