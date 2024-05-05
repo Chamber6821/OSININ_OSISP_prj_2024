3dc096cd25f11b95980a5e2a41d8a423d5392f1f
Chamber6821 <vania.osinin@yandex.ru>
2024-05-05 18:08:56 +0300

    feat(SfUnlimited): add throw if stack is empty

--- src/main/code/context/stack/SfUnlimited.hpp
+++ src/main/code/context/stack/SfUnlimited.hpp
@@ -4,12 +4,14 @@
 #include "java/value/JavaValue.hpp"
 #include "p.hpp"
 #include <stack>
+#include <stdexcept>
 
 class SfUnlimited : public StackFrame {
   std::stack<p<JavaValue>> stack;
 
 public:
   p<JavaValue> pop() override {
+    if (stack.empty()) throw std::runtime_error("Stack is already empty");
     auto val = stack.top();
     stack.pop();
     return val;
