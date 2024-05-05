1cd6904358ff0b6c91cd3f94fc8242f03c9cd40a
Chamber6821 <vania.osinin@yandex.ru>
2024-05-05 17:41:31 +0300

    refactor(InsAll): extract some common parts from instructions

--- src/main/code/instruction-set/InsAll.cpp
+++ src/main/code/instruction-set/InsAll.cpp
@@ -1,11 +1,16 @@
 #include "InsAll.hpp"
+#include "class-file/constant/CoInterfaceMethodRef.hpp"
 #include "class-file/constant/CoMethodRef.hpp"
 #include "class-file/constant/Constant.hpp"
 #include "class-file/constant/pool/ConstantPool.hpp"
+#include "code/Code.hpp"
+#include "code/MethodReference.hpp"
 #include "code/context/Context.hpp"
 #include "code/context/instruction-pointer/InstructionPointer.hpp"
+#include "code/context/stack/StackFrame.hpp"
 #include "code/instruction-set/InsWrap.hpp"
 #include "java/class/JavaClasses.hpp"
+#include "java/value/JavaValues.hpp"
 #include "java/value/JvsVector.hpp"
 #include "make.hpp"
 #include "p.hpp"
@@ -31,38 +36,40 @@ void jumpForward(p<InstructionPointer> pointer, int offset) {
   pointer->gotoAddress(pointer->address() + offset);
 }
 
+p<JavaValues> popArguments(p<StackFrame> stack, int count) {
+  auto arguments = make<JvsVector>(count);
+  for (int i = count - 1; i >= 0; i--) {
+    arguments->at(i) = stack->pop();
+  }
+  return arguments;
+}
+
 InsAll::InsAll(p<JavaClasses> classes)
     : InsMappedByOpcode(std::map<std::uint8_t, p<InstructionSet>>{
-        {0xB8,
-         make<InsWrap>([classes](
-                         std::span<const std::uint8_t> bytes,
-                         p<ConstantPool> pool
-                       ) {
-           auto methodConstant = verifyConstant<CoMethodRef>(
-             Constant::Tag::MethodRef,
-             pool->at(mergeBytes(bytes[0], bytes[1]))
-           );
-           return make<Code::Wrap>([classes,
-                                    methodConstant](p<Context> context, auto) {
-             auto argumentCount =
-               countArguments(methodConstant->type()->type()->value());
-             auto arguments = make<JvsVector>(argumentCount);
-             for (int i = argumentCount - 1; i >= 0; i--) {
-               arguments->at(i) = context->stack()->pop();
-             }
+        {0xB8, make<InsWrap>([=](auto bytes, p<ConstantPool> pool) {
+           auto method =
+             verifyConstant<CoMethodRef>(pool->at(mergeBytes(bytes[0], bytes[1])
+             ));
+           auto methodRef = MethodReference{
+             .name = method->type()->name()->value(),
+             .signature = method->type()->type()->value()
+           };
+           auto type = classes->type(method->clazz()->name()->value());
+           return make<Code::Wrap>([=](p<Context> context, auto) {
              jumpForward(context->instructionPointer(), 3);
              return Code::Call{
-               .type = classes->type(methodConstant->clazz()->name()->value()),
-               .method =
-                 {.name = methodConstant->type()->name()->value(),
-                  .signature = methodConstant->type()->type()->value()},
-               .arguments = arguments,
+               .type = type,
+               .method = methodRef,
+               .arguments = popArguments(
+                 context->stack(),
+                 countArguments(methodRef.signature)
+               ),
              };
            });
          })},
-        {0x12, make<InsWrap>([classes](auto bytes, p<ConstantPool> pool) {
+        {0x12, make<InsWrap>([=](auto bytes, p<ConstantPool> pool) {
            auto constant = valueOfConstant(pool->at(bytes[0]), classes);
-           return make<Code::Wrap>([constant](p<Context> context, auto) {
+           return make<Code::Wrap>([=](p<Context> context, auto) {
              context->stack()->push(constant);
              jumpForward(context->instructionPointer(), 2);
              return Code::Next{};
