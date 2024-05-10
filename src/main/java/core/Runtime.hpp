2d17380894e068066407e19ecb17e57dd47fb53e
Chamber6821 <vania.osinin@yandex.ru>
2024-05-10 19:04:05 +0300

    feat(Runtime): implement method suspend()

--- src/main/java/core/Runtime.hpp
+++ src/main/java/core/Runtime.hpp
@@ -2,11 +2,14 @@
 
 #include "code/Code.hpp"
 #include "code/MethodReference.hpp"
+#include "code/context/Context.hpp"
+#include "execution/task/Task.hpp"
 #include "java/class/JavaClass.hpp"
 #include "java/object/JavaObject.hpp"
 #include "java/value/JavaValue.hpp"
 #include "make.hpp"
 #include "p.hpp"
+#include "tool/Iterable.hpp"
 #include <format>
 #include <stdexcept>
 #include <string>
@@ -29,6 +32,13 @@ public:
     if (reference.equal("stdout", "()Lcore/Runtime$Ostream;"))
       return make<Code::Wrap>([this](auto, auto) {
         return Code::ReturnValue{make<JavaValue>(stdout)};
+      });
+    if (reference.equal("suspend", "()V"))
+      return make<Code::Wrap>([](p<Context> context, auto) -> Code::Result {
+        if (context->instructionPointer()->address() > 0)
+          return Code::ReturnVoid{};
+        context->instructionPointer()->gotoAddress(1);
+        return Code::ExecuteTasks{.tasks = make<Iterable<p<Task>>::Empty>()};
       });
     throw std::runtime_error(
       std::format("Class {} has no method {}", name(), reference)
