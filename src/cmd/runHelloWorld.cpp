a1088849e4c4da2f3d5cf5aa9ba0ed67364bf780
Chamber6821 <vania.osinin@yandex.ru>
2024-05-10 23:35:20 +0300

    refactor(runHelloWorld): use secondary constructor of JcFromClassFile

--- src/cmd/runHelloWorld.cpp
+++ src/cmd/runHelloWorld.cpp
@@ -1,6 +1,5 @@
 #include "class-file/CfParsed.hpp"
 #include "code/Code.hpp"
-#include "code/instruction-set/InsAll.hpp"
 #include "execution/machine/MaSingleThread.hpp"
 #include "execution/queue/QuStl.hpp"
 #include "execution/task/JavaTask.hpp"
@@ -17,16 +16,13 @@ int main() {
   try {
     auto classes = make<JcsSystem>();
     std::ifstream mainClass("resource/java/HelloWorld.class", std::ios::binary);
-    make<MaSingleThread>(make<QuStl>(make<JavaTask>(Code::Call{
-                           .type = make<JcFromClassFile>(
-                             make<CfParsed>(mainClass),
-                             classes->type("java/lang/Object"),
-                             classes,
-                             make<InsAll>(classes)
-                           ),
-                           .method = {"main", "([Ljava/lang/String;)V"},
-                           .arguments = make<JvsAutoExtendable>()
-                         })))
+    make<MaSingleThread>(
+      make<QuStl>(make<JavaTask>(Code::Call{
+        .type = make<JcFromClassFile>(make<CfParsed>(mainClass), classes),
+        .method = {"main", "([Ljava/lang/String;)V"},
+        .arguments = make<JvsAutoExtendable>()
+      }))
+    )
       ->run();
   } catch (const std::exception &e) {
     std::cout << stringify(e);
