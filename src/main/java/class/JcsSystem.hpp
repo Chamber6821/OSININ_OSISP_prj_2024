b468aa4821ee67b8beee320a7e2eb83e59c3330c
Chamber6821 <vania.osinin@yandex.ru>
2024-05-13 18:23:25 +0300

    feat(BoxClass): implement method toString

--- src/main/java/class/JcsSystem.hpp
+++ src/main/java/class/JcsSystem.hpp
@@ -6,7 +6,10 @@
 #include "java/lang/BoxClass.hpp"
 #include "java/lang/ObjectClass.hpp"
 #include "java/lang/StringClass.hpp"
+#include "java/value/JavaValue.hpp"
 #include "make.hpp"
+#include <cstdint>
+#include <string>
 
 class JcsSystem : public JcsMap {
 public:
@@ -16,7 +19,18 @@ public:
     auto stdoutClass = make<Stdout>(std::cout, objectClass);
     auto runtimeClass =
       make<Runtime>(stdoutClass->newObject(stdoutClass), objectClass);
-    auto integerClass = make<BoxClass>("java/lang/Integer", objectClass);
+    auto integerClass = make<BoxClass>(
+      "java/lang/Integer",
+      [](std::int32_t x) { return std::to_string(x); },
+      [=]() {
+        auto array = objectClass->newObject(objectClass);
+        array->setField("$length", make<JavaValue>(std::int32_t(0)));
+        auto string = stringClass->newObject(stringClass);
+        string->setField("$content", make<JavaValue>(std::move(array)));
+        return string;
+      },
+      objectClass
+    );
     add(objectClass->name(), objectClass);
     add(stringClass->name(), stringClass);
     add(stdoutClass->name(), stdoutClass);
--- src/main/java/lang/BoxClass.hpp
+++ src/main/java/lang/BoxClass.hpp
@@ -8,8 +8,10 @@
 #include "java/value/JavaValue.hpp"
 #include "make.hpp"
 #include "p.hpp"
+#include <cstddef>
 #include <exception>
 #include <format>
+#include <functional>
 #include <memory>
 #include <stdexcept>
 #include <string>
@@ -17,11 +19,65 @@
 class BoxClass : public JavaClass,
                  public std::enable_shared_from_this<BoxClass> {
   std::string _name;
+  std::function<p<JavaObject>(p<JavaValue>)> toString;
   p<JavaClass> super;
 
 public:
-  BoxClass(std::string name, p<JavaClass> super)
-      : _name(std::move(name)), super(std::move(super)) {}
+  BoxClass(
+    std::string name, std::function<p<JavaObject>(p<JavaValue>)> toString,
+    p<JavaClass> super
+  )
+      : _name(std::move(name)), toString(std::move(toString)),
+        super(std::move(super)) {}
+
+  BoxClass(
+    std::string name, std::function<std::string(p<JavaValue>)> toString,
+    std::function<p<JavaObject>()> emptyString, p<JavaClass> super
+  )
+      : BoxClass(
+          std::move(name),
+          [toString, emptyString](p<JavaValue> value) {
+            auto str = toString(std::move(value));
+            auto string = emptyString();
+            auto array = std::get<p<JavaObject>>(*string->field("$content"));
+            for (std::size_t i = 0; i < str.size(); i++) {
+              array->setField(
+                std::format("${}", i),
+                make<JavaValue>(std::int32_t(str[i]))
+              );
+            }
+            array->setField(
+              "$length",
+              make<JavaValue>(std::int32_t(str.size()))
+            );
+            return string;
+          },
+          std::move(super)
+        ) {}
+
+  template <class T>
+  BoxClass(
+    std::string name, std::function<std::string(T)> toString,
+    std::function<p<JavaObject>()> emptyString, p<JavaClass> super
+  )
+      : BoxClass(
+          std::move(name),
+          [toString](p<JavaValue> value) {
+            return toString(std::get<T>(*value));
+          },
+          std::move(emptyString), std::move(super)
+        ) {}
+
+  BoxClass(std::string name, auto toString, p<JavaClass> super)
+      : BoxClass(std::move(name), std::function(toString), std::move(super)) {}
+
+  BoxClass(
+    std::string name, auto toString, auto emptyString, p<JavaClass> super
+  )
+      : BoxClass(
+          std::move(name), std::function(toString), std::function(emptyString),
+          std::move(super)
+        ) {}
 
   std::string name() const override { return _name; }
 
@@ -36,6 +92,12 @@ public:
           newObject(std::const_pointer_cast<BoxClass>(shared_from_this()));
         object->setField("$content", context->locals()->at(0));
         return Code::ReturnValue{make<JavaValue>(std::move(object))};
+      });
+    if (method.equal("toString", "()Ljava/lang/String;"))
+      return make<Code::Wrap>([this](p<Context> context) {
+        return Code::ReturnValue{make<JavaValue>(toString(
+          std::get<p<JavaObject>>(*context->locals()->at(0))->field("$content")
+        ))};
       });
     return super->methodCode(std::move(method));
   } catch (...) {
