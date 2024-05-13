7f86549f7efb1af098cd47104cea4656914ec9d4
Chamber6821 <vania.osinin@yandex.ru>
2024-05-13 18:34:47 +0300

    feat(JoMap): throw exception if attribute not defined

--- src/main/java/object/JoMap.hpp
+++ src/main/java/object/JoMap.hpp
@@ -4,7 +4,10 @@
 #include "java/object/JavaObject.hpp"
 #include "java/value/JavaValue.hpp"
 #include "p.hpp"
+#include <cstdint>
+#include <format>
 #include <map>
+#include <stdexcept>
 #include <string>
 
 class JoMap : public JavaObject {
@@ -16,7 +19,15 @@ public:
 
   p<JavaClass> type() const override { return _type; }
 
-  p<JavaValue> field(std::string name) const override { return map.at(name); }
+  p<JavaValue> field(std::string name) const override {
+    if (not map.contains(name))
+      throw std::runtime_error(std::format(
+        "Object {:X} has no attribute {}",
+        (std::intptr_t)this,
+        name
+      ));
+    return map.at(name);
+  }
 
   void setField(std::string name, p<JavaValue> value) override {
     map[name] = value;
